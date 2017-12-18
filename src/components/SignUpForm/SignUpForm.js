5aa9636e981aa0144059e8b818af4812cf8b9c28
Alex Wong <alexw14@gmail.com>
2017-12-17 17:42:34 -0800

    Modify SignUpForm into functional stateless component

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,97 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import userService from '../../utils/userService';
 
-class SignUpForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      passwordConf: '',
-      biography: '',
-      website: '',
-      avatar: ''
-    }
-  }
+const SignUpForm = (props) => {
 
-  handleChange = (field, e) => {
-    this.props.updateMessage('');
-    this.setState({
-      [field]: e.target.value
-    });
-  }
+  let button = <button type='submit' className='btn' disabled>Create My Account</button>;
+  if (props.isFormValid()) button = <button type='submit' className='btn'>Create My Account</button>;
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    userService.signup(this.state)
-      // if successfully signed up
-      .then(() => {
-        this.props.handleSignUp();
-        this.props.history.push('/');
-      })
-      .catch(err => this.props.updateMessage(err.message));
-  }
-
-  isFormValid() {
-    if (this.state.password === this.state.passwordConf && this.state.passwordConf !== '') return true;
-  }
-
-  render() {
-    let button = <button type='submit' className='btn' disabled>Create My Account</button>;
-    if (this.isFormValid()) button = <button type='submit' className='btn'>Create My Account</button>;
-
-    return (
-      <div>
-        <header><h1>Sign Up</h1></header>
-        <form onSubmit={this.handleSubmit}>
-          <div className='container'>
-            <div className='row'>
-              <div className='input-field col s6'>
-                <input type='text' onChange={(e) => this.handleChange('name', e)} />
-                <label>Name</label>
-              </div>
-              <div className='input-field col s6'>
-                <input type='text' onChange={(e) => this.handleChange('email', e)} />
-                <label>Email</label>
-              </div>
+  return (
+    <div>
+      <header><h1>Sign Up</h1></header>
+      <form onSubmit={props.handleSubmit}>
+        <div className='container'>
+          <div className='row'>
+            <div className='input-field col s6'>
+              <input type='text' onChange={(e) => props.handleChange('name', e)} />
+              <label>Name</label>
             </div>
-            <div className='row'>
-              <div className='input-field col s6'>
-                <input type='password' onChange={(e) => this.handleChange('password', e)} />
-                <label>Password</label>
-              </div>
-              <div className='input-field col s6'>
-                <input type='password' onChange={(e) => this.handleChange('passwordConf', e)} />
-                <label>Password Confirmation</label>
-              </div>
+            <div className='input-field col s6'>
+              <input type='text' onChange={(e) => props.handleChange('email', e)} />
+              <label>Email</label>
             </div>
-            <div className='row'>
-              <div className='input-field col s12'>
-                <textarea className='materialize-textarea' onChange={(e) => this.handleChange('biography', e)}></textarea>
-                <label>Biography</label>
-              </div>
-            </div>
-            <div className='row'>
-              <div className='input-field col s6'>
-                <input type='text' onChange={(e) => this.handleChange('website', e)} />
-                <label>Website URL</label>
-              </div>
-              <div className='input-field col s6'>
-                <input type='text' onChange={(e) => this.handleChange('avatar', e)} />
-                <label>Profile Picture URL</label>
-              </div>
-            </div>
-            <div>
-              {button}
-            </div>
-            <div style={{ margin: '25px 0' }}> <Link to='/'>Cancel</Link> </div>
           </div>
-        </form>
-      </div>
-    )
-  }
+          <div className='row'>
+            <div className='input-field col s6'>
+              <input type='password' onChange={(e) => props.handleChange('password', e)} />
+              <label>Password</label>
+            </div>
+            <div className='input-field col s6'>
+              <input type='password' onChange={(e) => props.handleChange('passwordConf', e)} />
+              <label>Password Confirmation</label>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='input-field col s12'>
+              <textarea className='materialize-textarea' onChange={(e) => props.handleChange('biography', e)}></textarea>
+              <label>Biography</label>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='input-field col s6'>
+              <input type='text' onChange={(e) => props.handleChange('website', e)} />
+              <label>Website URL</label>
+            </div>
+            <div className='input-field col s6'>
+              <input type='text' onChange={(e) => props.handleChange('avatar', e)} />
+              <label>Profile Picture URL</label>
+            </div>
+          </div>
+          <div>
+            {button}
+          </div>
+          <div style={{ margin: '25px 0' }}> <Link to='/'>Cancel</Link> </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default SignUpForm;
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import userService from '../../utils/userService';
 
 class SignUpPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {message: ''}
+    this.state = {
+      message: '',
+      email: '',
+      password: '',
+      passwordConf: '',
+      biography: '',
+      website: '',
+      avatar: ''
+    }
   }
-
+  
   updateMessage = (msg) => {
     this.setState({message: msg});
+  }
+
+  handleChange = (field, e) => {
+    this.updateMessage('');
+    this.setState({
+      [field]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    userService.signup(this.state)
+      // if successfully signed up
+      .then(() => {
+        this.props.handleSignUp();
+        this.props.history.push('/');
+      })
+      .catch(err => this.props.updateMessage(err.message));
+  }
+
+  isFormValid = () => {
+    if (this.state.password === this.state.passwordConf && this.state.passwordConf !== '') return true;
   }
 
   render() {
@@ -17,13 +48,15 @@ class SignUpPage extends Component {
         <SignUpForm
           {...this.props}
           updateMessage={this.updateMessage}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          isFormValid={this.isFormValid}
           handleSignUp={this.props.handleSignUp} 
         />
         <p>{this.state.message}</p>
       </div>
     )
   }
-
 }
 
 export default SignUpPage;
