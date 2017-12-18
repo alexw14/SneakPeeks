5b5e4da3b9fda8dc321b7718f436cce3beaa5270
Alex Wong <alexw14@gmail.com>
2017-12-17 18:50:36 -0800

    Modify LoginForm into functional stateless component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,60 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import userService from '../../utils/userService';
 
-class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
-
-  handleChange = (field, e) => {
-    this.props.updateMessage('');
-    this.setState({
-      [field]: e.target.value
-    });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    userService.login(this.state)
-      .then(() => {
-        this.props.handleLogin();
-        this.props.history.push('/');
-      })
-      .catch(err => this.props.updateMessage(err.message));
-  }
-
-  render() {
-    return (
-      <div>
-        <header><h1>Log In</h1></header>
-        <form onSubmit={this.handleSubmit}>
-          <div className='container'>
-            <div className='row'>
-              <div className='input-field col s6 offset-s3'>
-                <input type='text' onChange={(e) => this.handleChange('email', e)} />
-                <label>Email</label>
-              </div>
+const LoginForm = (props) => {
+  return (
+    <div>
+      <header><h1>Log In</h1></header>
+      <form onSubmit={props.handleSubmit}>
+        <div className='container'>
+          <div className='row'>
+            <div className='input-field col s6 offset-s3'>
+              <input type='text' onChange={(e) => props.handleChange('email', e)} />
+              <label>Email</label>
             </div>
-            <div className='row'>
-              <div className='input-field col s6 offset-s3'>
-                <input type='password' onChange={(e) => this.handleChange('password', e)} />
-                <label>Password</label>
-              </div>
-            </div>
-            <div>
-              <button type='submit' className='btn'>Log In</button>
-            </div>
-            <div style={{ margin: '25px 0' }}> <Link to='/'>Cancel</Link> </div>
           </div>
-        </form>
-      </div>
-    )
-  }
+          <div className='row'>
+            <div className='input-field col s6 offset-s3'>
+              <input type='password' onChange={(e) => props.handleChange('password', e)} />
+              <label>Password</label>
+            </div>
+          </div>
+          <div>
+            <button type='submit' className='btn'>Log In</button>
+          </div>
+          <div style={{ margin: '25px 0' }}> <Link to='/'>Cancel</Link> </div>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default LoginForm;
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import userService from '../../utils/userService';
 
 class LoginPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {message: ''}
+    this.state = {
+      message: '',
+      email: '',
+      password: ''
+    }
   }
 
   updateMessage = (msg) => {
     this.setState({message: msg});
+  }
+
+  handleChange = (field, e) => {
+    this.updateMessage('');
+    this.setState({
+      [field]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    userService.login(this.state)
+      .then(() => {
+        this.props.handleLogin();
+        this.props.history.push('/');
+      })
+      .catch(err => this.updateMessage(err.message));
   }
 
   render() {
@@ -17,7 +39,8 @@ class LoginPage extends Component {
         <LoginForm
           {...this.props}
           updateMessage={this.updateMessage}
-          handleLogin={this.props.handleLogin} 
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit} 
         />
         <p>{this.state.message}</p>
       </div>
