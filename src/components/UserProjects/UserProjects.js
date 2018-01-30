6724e57fed27917004afa5fecb06198782fea0df
Alex Wong <alexw14@gmail.com>
2018-01-29 19:44:24 -0800

    Modify ProfilePage

--- src/components/UserProjects/UserProjects.js
+++ src/components/UserProjects/UserProjects.js
@@ -3,8 +3,8 @@ import ProjectCard from '../ProjectCard/ProjectCard';
 
 const UserProjects = (props) => {
 
-  let projectList = props.projects ? 
-    props.projects.map(project => <ProjectCard key={project._id} project={project} />)
+  let projectList = props.projects ?
+    props.projects.map(project => <ProjectCard key={project._id} project={project} numberWithCommas={props.numberWithCommas} />)
     :
     <h1>Loading</h1>
 
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ProfileDisplay from '../../components/ProfileDisplay/ProfileDisplay';
 import UserProjects from '../../components/UserProjects/UserProjects';
 
-const ProfilePage = (props) => {
-  return (
-    <div className='container'>
-      <ProfileDisplay 
-        user={props.user}
-      />
-      <UserProjects
-        projects={props.projects}
-      />
-    </div>
-  )
+class ProfilePage extends Component {
+
+  numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <ProfileDisplay
+          user={this.props.user}
+        />
+        <UserProjects
+          projects={this.props.projects}
+          numberWithCommas={this.numberWithCommas}
+        />
+      </div>
+    )
+  }
 }
 
 export default ProfilePage;
